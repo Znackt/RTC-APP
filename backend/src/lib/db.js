f70adc444f9e9227958d90e07273e6ce8eaf5251
Znackt <Znackt@proton.me>
2025-01-20 02:44:39 +0530

    Created a db.js file for database connection

---
+++ backend/src/lib/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+export const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB connected: ${connect.connection.host}`);
+    } catch (e) {
+        console.log("MongoDB connection error: ", e)
+    }
+}
