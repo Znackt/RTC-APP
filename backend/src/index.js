62371f1294d2508938b9a2991be43b8fd1ed838c
Znackt <Znackt@proton.me>
2025-01-20 14:03:34 +0530

    using cookie-parser for parsing cookies

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,19 @@
-import express from "express";
 import authRoutes from "./routes/auth.route.js"
+import cookieParser from "cookie-parser";
+import express from "express";
 import dotenv from "dotenv"
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
-const PORT = process.env.PORT;
-
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 
-app.listen(PORT, () => {
-    console.log(`App is listening on PORT: ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`App is listening on PORT: ${process.env.PORT}`);
     connectDB();
 })
