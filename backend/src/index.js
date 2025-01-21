d5a8c2000c5abf183aed438aa637d380986f5527
Znackt <Znackt@proton.me>
2025-01-21 21:08:11 +0530

    added messageRoutes ?

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,22 @@
-import authRoutes from "./routes/auth.route.js"
 import cookieParser from "cookie-parser";
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 import { connectDB } from "./lib/db.js";
+
+import messageRoutes from "./model/message.model.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("api/message", messageRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`App is listening on PORT: ${process.env.PORT}`);
-    connectDB();
-})
+  console.log(`App is listening on PORT: ${process.env.PORT}`);
+  connectDB();
+});
