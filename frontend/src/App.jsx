ea7219873ec4391aa328a349f48814693ae7b09a
Znackt <Znackt@proton.me>
2025-01-24 19:12:58 +0530

    Created pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import Settings from "./pages/Settings.jsx";
+import Profile from "./pages/Profile.jsx"
+import SignUp from "./pages/SignUp.jsx";
+import Login from "./pages/Login.jsx";
 const App = () => {
   return (
-    <div className="">
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 };
