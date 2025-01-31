93a86bf3970263007a5e3ba30920e77eb2d730ec
Znackt <Znackt@proton.me>
2025-02-01 00:29:08 +0530

    Created the login function

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -23,7 +23,7 @@ export const useAuthStore = create((set) => ({
   },
 
   signup: async (data) => {
-    set({ isSigningUp: true })
+    set({ isSigningUp: true });
     try {
       const res = await axiosInstance.post("/auth/signup", data);
       set({ authUser: res.data });
@@ -42,5 +42,18 @@ export const useAuthStore = create((set) => ({
     } catch (error) {
       toast.error(error.response.data.message);
     }
-  }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
 }));
