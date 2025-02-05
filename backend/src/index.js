42873dcaadb243a006f019426ea6aeaa43cff08d
Znackt <Znackt@proton.me>
2025-02-05 23:32:07 +0530

    added limits to express.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,8 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb', extended: true }));
+app.use(express.urlencoded({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(
   cors({
@@ -19,6 +20,7 @@ app.use(
     credentials: true,
   })
 );
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
