2c01205c376f30242542cccdf7521fdb6e8813e4
Znackt <Znackt@proton.me>
2025-02-06 08:48:49 +0530

    added extended: true

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json({ limit: '10mb', extended: true }));
-app.use(express.urlencoded({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb' , extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
