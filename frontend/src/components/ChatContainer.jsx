b14cada7e59799e5c2cffaa4a88810996e4770e0
Znackt <Znackt@proton.me>
2025-02-11 21:53:11 +0530

    Removed the comments

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -72,22 +72,3 @@ const ChatContainer = () => {
 };
 
 export default ChatContainer;
-
-/*
-<div className="chat-header mb-1">
-              <time className="text-xs opacity-50 ml-1">
-                {formatMessageTime(message.createdAt)}
-              </time>
-            </div>
-            <div className="chat-bubble flex flex-col">
-              {message.image && (
-                <img
-                  src={message.image}
-                  alt="Attachment"
-                  className="sm:max-w-[200px] rounded-md mb-2"
-                />
-              )}
-              {message.text && <p>{message.text}</p>}
-            </div>
-
-*/
