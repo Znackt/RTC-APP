c04d90519a0cfa43f50371c1830094858501b95e
Znackt <Znackt@proton.me>
2025-03-04 03:03:32 +0530

    Re-coonfigure CORS v1.2

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,17 +15,16 @@ dotenv.config();
 
 const __dirname = path.resolve();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://rtc-app-mu.vercel.app"], 
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true,  
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json({ limit: '20mb', extended: true }));
 app.use(express.urlencoded({ limit: '20mb' , extended: true }));
 app.use(cookieParser());
-  
-app.use(cors({
-  origin: ["http://localhost:5173", "https://rtc-app-mu.vercel.app"], // Allow your frontend origins
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  credentials: true,  // ✅ This is important if you’re sending cookies or authentication headers
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
