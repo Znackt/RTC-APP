9d00629ef1a1f83ce2b113ac9c69adff8c4d9a59
Znackt <Znackt@proton.me>
2025-03-04 02:52:45 +0530

    Tryna fix CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,24 +15,14 @@ dotenv.config();
 
 const __dirname = path.resolve();
 
-app.use(cors());
 app.use(express.json({ limit: '20mb', extended: true }));
 app.use(express.urlencoded({ limit: '20mb' , extended: true }));
 app.use(cookieParser());
-
-// Allowed origins
-// const whitelist = [
-//   "http://localhost:5173",
-//   "https://rtc-app-mu.vercel.app",
-// ];
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*" );
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
+  
+app.use(cors({
+  origin: ["http://localhost:5173", "https://rtc-app-mu.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
