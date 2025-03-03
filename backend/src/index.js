a52840f875589410e64d9b8e65f1337039d8d800
Znackt <Znackt@proton.me>
2025-03-04 02:24:19 +0530

    huh

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,22 +15,24 @@ dotenv.config();
 
 const __dirname = path.resolve();
 
+app.use(cors());
 app.use(express.json({ limit: '20mb', extended: true }));
 app.use(express.urlencoded({ limit: '20mb' , extended: true }));
 app.use(cookieParser());
-app.use(cors());
 
-// Allowed origins
-const whitelist = [
-  "http://localhost:5173",
-  "https://rtc-app-mu.vercel.app",
-];
+// // Allowed origins
+// const whitelist = [
+//   "http://localhost:5173",
+//   "https://rtc-app-mu.vercel.app",
+// ];
 
-app.use(cors({
-  origin: "*",
-  credentials: true,
-}));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
