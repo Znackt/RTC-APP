e72594445faf1ba2b8adf9bf4c775f416c1481c8
Znackt <Znackt@proton.me>
2025-03-22 18:16:54 +0530

    Changed the backendURL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5050/api" : "https://rtc-app-backend.vercel.app/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5050/api" : "https://rtc-app-jhm2.vercel.app/api",
   withCredentials: true,
 });
